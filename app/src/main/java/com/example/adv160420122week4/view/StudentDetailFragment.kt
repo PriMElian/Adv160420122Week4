e422cfe169dbde0af49b9908c2e21279eed07944
priMElian <komangelianz25@gmail.com>
2023-03-26 23:54:30 +0700

    init

--- app/src/main/java/com/example/adv160420122week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/adv160420122week4/view/StudentDetailFragment.kt
@@ -5,14 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.adv160420122week4.R
+import com.example.adv160420122week4.viewmodel.DetailViewModel
+import com.example.adv160420122week4.viewmodel.ListViewModel
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        val txtID = view.findViewById<TextView>(R.id.txtID)
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+
     }
 }
